7cf3b1e356689b483e4fa59e2d03adfaedcd7e13
Oleksandr <bragardlin@gmail.com>
2023-05-11 20:25:49 +0300

    added lazy-load 2

--- src/js/slideTmpl.js
+++ src/js/slideTmpl.js
@@ -34,7 +34,7 @@ export default function (data) {
       <button class="hero__btn js-hero-trailer" type="button" data-id="${id}">Watch trailer</button>
       </div>
       <div class="hero__image-wrap">
-      <img class="hero__image" src="https://image.tmdb.org/t/p/original${backdrop_path}" alt="hero">
+      <img class="hero__image" src="https://image.tmdb.org/t/p/original${backdrop_path}" alt="hero" loading="lazy">
       </div>
       </div>`;
     })
