224623d4fdb8682e201e2bf77444b110a511e844
Vadim Ilchuk <vadmany@gmail.com>
2023-05-12 12:09:59 +0200

    fix_html

--- src/index.js
+++ src/index.js
@@ -1,14 +1,9 @@
-import './js/api';
-// import './js/weeklytrends';
 import './js/library-fetch';
-// import './js/popular-films-fetch';
 import './js/team-modal';
-// import './js/fetch-one-film';
 import './js/fetch-video-key';
 import './js/them-switcher';
 import './js/button-up';
 import './js/refs';
-
 import './js/details-modal';
 import './js/local-storage';
 import './js/hero';
@@ -18,6 +13,5 @@ import './js/film-card';
 import './js/trailer';
 import './js/catalog-modal-close';
 import './js/movie-found';
-
 import './js/catalog';
 import './js/mobile-menu';
